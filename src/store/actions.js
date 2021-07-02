a5c85ce3a6157200117ce2003300a850d9fd9630
Andrés <usuario616@gmail.com>
2021-07-03 01:05:59 +0200

    Added username to state and it's showed in welcome page

--- src/store/actions.js
+++ src/store/actions.js
@@ -7,15 +7,17 @@ import {
 
 import { login } from '../api/auth';
 
-export const authLoginRequest = () => {
+export const authLoginRequest = username => {
   return {
     type: AUTH_LOGIN_REQUEST,
+    payload: username,
   };
 };
 
-export const authLoginSuccess = () => {
+export const authLoginSuccess = username => {
   return {
     type: AUTH_LOGIN_SUCCESS,
+    payload: username,
   };
 };
 
@@ -31,10 +33,11 @@ export const loginAction = (credentials, history, location) => {
   return async function (dispatch, getState) {
     dispatch(authLoginRequest());
     try {
-      await login(credentials);
-      dispatch(authLoginSuccess());
+      console.log(credentials);
+      const username = await login(credentials);
+      dispatch(authLoginSuccess(username));
       // Redirect
-      const { from } = location.state || { from: { pathname: '/' }}
+      const { from } = location.state || { from: { pathname: '/' } };
       history.replace(from);
     } catch (error) {
       dispatch(authLoginFailure(error));
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,1 +1,2 @@
-export const getIsLogged = state => state.auth;
+export const getIsLogged = state => state.auth.logged;
+export const getUsername = state => state.auth.username;
