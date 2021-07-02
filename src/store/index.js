41eebcd078e94921c709b6d562cadff0dcd28294
JC <jcmpes@gmail.com>
2021-07-02 09:29:34 +0200

    Added reducer for ui and use combineReducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
-import reducer from './reducers';
+import * as reducers from './reducers';
 
 const middleware = [thunk];
 
 const configureStore = ({ preloadedState }) => {
   const store = createStore(
-    reducer,
+    combineReducers(reducers),
     preloadedState,
     composeWithDevTools(applyMiddleware(...middleware)),
   );
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -3,6 +3,7 @@ import {
   AUTH_LOGIN_SUCCESS,
   AUTH_LOGIN_FAILURE,
   AUTH_LOGOUT,
+  UI_RESET_ERROR
 } from './types';
 
 export const initialState = {
@@ -15,6 +16,26 @@ export function auth(state = initialState.auth, action) {
       return true;
     case AUTH_LOGOUT:
       return false;
+    default:
+      return state;
+  }
+}
+
+export function ui(state = initialState.ui, action) {
+  // case AUTH_LOGIN_FAILURE managed with if statement
+  if (action.error) {
+    return { ...state, loading: false, error: action.payload };
+  }
+  switch (action.type) {
+    case AUTH_LOGIN_REQUEST:
+      return { ...state, loading: true, error: null };
+    case AUTH_LOGIN_SUCCESS:
+      return { ...state, loading: false };
+    case UI_RESET_ERROR:
+      return {
+        ...state,
+        error: null,
+      };
     default:
       return state;
   }
