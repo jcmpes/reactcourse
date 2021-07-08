ab4ee37b09b14e30447f9caaa77cafe2d38c098c
oscarCorb <info@oscarcorb.com>
2021-07-08 13:42:44 +0200

    feat: i18next-browser-languagedetector installed and configured

--- src/config/i18next-config.js
+++ src/config/i18next-config.js
@@ -1,25 +1,28 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-i18n.use(initReactI18next).init({
-  lng: 'en',
-  fallbackLng: 'en',
-  resources: {
-    en: {
-      translation: {
-        title: 'CourseApp',
-        welcome: 'Welcome to ',
-        headline: 'We hope you survive the experience.',
-        notFound: 'Not found page',
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    resources: {
+      en: {
+        translation: {
+          title: 'CourseApp',
+          welcome: 'Welcome to ',
+          headline: 'We hope you survive the experience.',
+          notFound: 'Not found page',
+        },
+      },
+      es: {
+        translation: {
+          title: 'CourseApp',
+          welcome: 'Bienvenido a ',
+          headline: 'Esperamos que sobrevivas a la experiencia.',
+          notFound: 'Página no encontrada',
+        },
       },
     },
-    es: {
-      translation: {
-        title: 'CourseApp',
-        welcome: 'Bienvenido a ',
-        headline: 'Esperamos que sobrevivas a la experiencia.',
-        notFound: 'Página no encontrada',
-      },
-    },
-  },
-});
+  });
