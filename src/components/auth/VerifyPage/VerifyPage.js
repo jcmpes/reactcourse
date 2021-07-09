2e33d44789d4a0c217cd12ed27f54ffd0c94d3da
Manuel Hernandez <cmanuelhm87@gmail.com>
2021-07-09 17:41:54 +0200

    Fix: Fix add verifyToken to useEfffect

--- src/components/auth/VerifyPage/VerifyPage.js
+++ src/components/auth/VerifyPage/VerifyPage.js
@@ -1,30 +1,35 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-function VerifyPage () {
-  const { verifyToken } = useParams()
-  const [verified, setVerified] = useState(false)
+function VerifyPage() {
+  const { verifyToken } = useParams();
+  const [verified, setVerified] = useState(false);
 
   useEffect(() => {
     // GET request to backend to verify the user
-    fetch(`${process.env.REACT_APP_API_BASE_URL}api/v1/verify?vt=${verifyToken}`)
-      .then(response => { 
+    fetch(
+      `${process.env.REACT_APP_API_BASE_URL}api/v1/verify?vt=${verifyToken}`,
+    )
+      .then((response) => {
         if (response.status === 200) {
-          setVerified(true)
-        }})
-      .catch(err => console.log(err))
-  }, [])
+          setVerified(true);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, [verifyToken]);
   return (
     <div className="verify-page">
       Bienvenido,
-      {verified
-        ? <>
+      {verified ? (
+        <>
           <p>HAS ACTIAVDO TU USUARIO</p>
           <a href="/login">Ir a Log In</a>
-        </> 
-        : ' lo sentimos, pero algo no ha ido bien. Tu usuario no esta activado, vuelve a intentarlo.'}
+        </>
+      ) : (
+        ' lo sentimos, pero algo no ha ido bien. Tu usuario no esta activado, vuelve a intentarlo.'
+      )}
     </div>
-  )
+  );
 }
 
 export default VerifyPage;
