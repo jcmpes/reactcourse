aa3d7eead508f466bfc277891cbccb87fda9a33f
Manuel Hernandez <cmanuelhm87@gmail.com>
2021-07-11 16:52:31 +0200

    Feat: Added Light/Dark mode into the layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import MyButton from '../shared/MyButton';
 
 import { Header, Footer } from '../layout';
 import './Layout.css';
 
 function Layout({ children, ...props }) {
+  const storedDarkMode = localStorage.getItem('DARK_MODE');
+  const [darkMode, setDarkMode] = React.useState(storedDarkMode);
+  const toggleDarkMode = () => setDarkMode(darkMode ? false : true);
+
+  React.useEffect(() => {
+    localStorage.setItem('DARK_MODE', darkMode);
+  }, [darkMode]);
+
   return (
     <div className="layout">
       <Header {...props} />
+      <MyButton onClick={toggleDarkMode}>
+        {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+      </MyButton>
       <hr />
-      <main>{children}</main>
+      <div className="App" data-theme={darkMode ? 'dark' : 'light'}>
+        <main>{children}</main>
+      </div>
       <hr />
       <Footer />
     </div>
