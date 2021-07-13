50c899f68b5f25da4de2d9649449b8a8893b246a
oscarCorb <info@oscarcorb.com>
2021-07-13 20:00:08 +0200

    feat: language buttons in header

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import './Footer.css';
 
 const Footer = () => {
+  const { t } = useTranslation(['global']);
+
   return (
     <footer>
       <div>
-        <p>Pie de página</p>
+        <p>{t('footer.footer')}</p>
       </div>
     </footer>
   );
--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,9 +1,35 @@
+import { useTranslation } from 'react-i18next';
 import AuthButton from '../../auth/AuthButton';
+import { Button } from '../../shared';
+import MyButton from '../../shared/MyButton';
 import './Header.css';
 
 const Header = ({ isLogged, onLogout, ...props }) => {
+  const { t, i18n } = useTranslation(['global']);
+  const switchLanguage = (ev) => {
+    if (ev.target.innerHTML === 'es') {
+      i18n.changeLanguage('es');
+    } else if (ev.target.innerHTML === 'en') {
+      i18n.changeLanguage('en');
+    }
+  };
+
   return (
     <header className="header" {...props}>
+      <a href="/">
+        <Button>{t('header.home')}</Button>
+      </a>
+      <a href="/user">
+        <Button>{t('header.user')}</Button>
+      </a>
+
+      <Button type="text" onClick={switchLanguage}>
+        en
+      </Button>
+      <Button type="text" onClick={switchLanguage}>
+        es
+      </Button>
+
       <AuthButton
         className="header-button"
         isLogged={isLogged}
