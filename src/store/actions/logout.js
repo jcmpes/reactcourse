2500897b2b3a7ce7ed290f4a3e194aba6e4e92ec
JC <jcmpes@gmail.com>
2021-07-15 15:45:53 +0200

    feat: upload image with multer

--- src/store/actions/logout.js
+++ src/store/actions/logout.js
@@ -1,7 +1,9 @@
+import { resetClient } from '../../api/client';
 import { AUTH_LOGOUT } from '../types';
 
 // Log out action
 export const authLogout = () => {
+  resetClient()
   return {
     type: AUTH_LOGOUT,
   };
