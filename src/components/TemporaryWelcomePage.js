9b0db16b2e461c9eaf1325b4123a3bd8516b83ba
oscarCorb <info@oscarcorb.com>
2021-07-16 12:06:01 +0200

    clean old stuff of the temporarywelcome component

--- src/components/TemporaryWelcomePage.js
+++ src/components/TemporaryWelcomePage.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { logout } from '../api/auth';
 import { authLogout } from '../store/actions/logout';
 import { loadCoursesAction } from '../store/actions/load-courses';
 import { getAuth, getUI } from '../store/selectors';
 import Layout from './layout/Layout';
-import { Button } from '../components/shared';
 import { useTranslation } from 'react-i18next';
 import { getCourses } from '../api/courses';
 import Course from '../components/courses/Course';
@@ -19,30 +16,7 @@ function TemporaryWelcomePage({ auth, onLogout, ...props }) {
   const { loading, error } = useSelector(getUI);
   const dispatch = useDispatch();
 
-  const handleLogoutClick = () => {
-    logout().then(onLogout);
-  };
-
-  const switchLanguage = (ev) => {
-    if (ev.target.innerHTML === 'Español') {
-      i18n.changeLanguage('es');
-    } else if (ev.target.innerHTML === 'English') {
-      i18n.changeLanguage('en');
-    }
-  };
-
-  const propsButton = { onClick: handleLogoutClick, children: 'Log Out' };
-
-  const propsLoginLink = {
-    to: '/login',
-    children: 'Log In',
-  };
-  const propsRegisterLink = {
-    to: '/register',
-    children: 'Register',
-  };
-
-  const { isLogged, username } = auth;
+  const { username } = auth;
 
   const [courses, setCourses] = React.useState([]);
   React.useEffect(() => {
@@ -79,16 +53,10 @@ function TemporaryWelcomePage({ auth, onLogout, ...props }) {
 
       <div>{t('headline')}</div>
 
-      {isLogged ? <Button {...propsButton} /> : <Link {...propsLoginLink} />}
-      <br />
-      {!isLogged && <Link {...propsRegisterLink} />}
-
       <p>
         Current language: <strong>{i18n.language}</strong>
       </p>
-      <Button children="English" onClick={switchLanguage} />
-      <Button children="Español" onClick={switchLanguage} />
-      
+
       {coursesElement.length === 0 && !loading
         ? "There's no courses yet"
         : coursesElement}
