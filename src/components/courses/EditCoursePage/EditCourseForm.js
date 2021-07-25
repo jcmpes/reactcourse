d868a607ddb52d15d2d4e2e811da6136e2d69973
JC <jcmpes@gmail.com>
2021-07-25 19:44:49 +0200

    fix: last warning

--- src/components/courses/EditCoursePage/EditCourseForm.js
+++ src/components/courses/EditCoursePage/EditCourseForm.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { useTranslation } from "react-i18next";
-import { getAuth } from "../../../store/selectors";
 import { FormField, Button, Input } from "../../../components/shared";
 import FileUpload from '../../shared/FileUpload';
 
