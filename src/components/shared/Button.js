ed0f8109cbe27bd8aa74f67171f503e14f267e45
oscarCorb <info@oscarcorb.com>
2021-07-30 00:42:22 +0200

    feat: buttons styles

--- src/components/shared/Button.js
+++ src/components/shared/Button.js
@@ -1,5 +1,11 @@
+import styles from './Button.module.css';
+
 function Button({ children, ...props }) {
-  return <button {...props}>{children}</button>;
+  return (
+    <button className={styles.button} {...props}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
