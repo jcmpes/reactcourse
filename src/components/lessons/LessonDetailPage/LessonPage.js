acf411d3e8c206b7db3e77902802e12b30369f92
JC <jcmpes@gmail.com>
2021-07-30 14:36:30 +0200

    feat: can navigatte fron lesson 1 to lesson 2

--- src/components/lessons/LessonDetailPage/LessonPage.js
+++ src/components/lessons/LessonDetailPage/LessonPage.js
@@ -1,29 +1,43 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { getCourse } from '../../../api/courses';
 import { getLesson } from '../../../api/lessons';
 import { getUi } from '../../../store/selectors';
 import Layout from '../../layout/Layout';
+import { Button } from '../../shared';
 import LessonDetail from './LessonDetail';
 
 function LessonPage() {
   const { courseSlug, lessonSlug } = useParams();
   const { loading } = useSelector(getUi);
   const [lesson, setLesson] = useState()
+  const [course, setCourse] = useState()
+  const [lessonCount, setLessonCount] = useState();
+
 
   React.useEffect(() => {
     const fetchData = async () =>  {
+      setCourse(await getCourse(courseSlug))
       setLesson(await getLesson(courseSlug, lessonSlug))
     }
     fetchData()
-    console.log(lesson)
-  }, [courseSlug]);
+  }, [lessonSlug]);
 
   return (
     <Layout>
       <div className="lesson-detail-page">
         {loading && "I'm loading..."}
-        {lesson && <LessonDetail {...lesson} />}
+        {lesson && 
+          <>
+            <LessonDetail {...lesson} />
+            <div className="lesson-nav">
+              <Link to={`/courses/${courseSlug}/${course.lessons[1].slug}`}>
+                <Button>Next lesson</Button>
+              </Link>
+            </div>
+          </>
+        }
       </div>
     </Layout>
   );
