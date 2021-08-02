97013f8ad84d3da773021c88aaa6e7ece0674217
JC <jcmpes@gmail.com>
2021-08-02 10:02:24 +0200

    fix: navigate between course presentation and first lesson

--- src/components/lessons/LessonDetailPage/LessonPage.js
+++ src/components/lessons/LessonDetailPage/LessonPage.js
@@ -1,10 +1,9 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Link, Redirect, useParams } from 'react-router-dom';
+import { Redirect, useHistory, useParams } from 'react-router-dom';
 import { getCourse } from '../../../api/courses';
 import { getLesson } from '../../../api/lessons';
 import { getUi } from '../../../store/selectors';
-import CoursesList from '../../courses/CoursesList';
 import Layout from '../../layout/Layout';
 import { Button } from '../../shared';
 import LessonDetail from './LessonDetail';
@@ -15,6 +14,7 @@ function LessonPage() {
   const [lesson, setLesson] = useState()
   const [course, setCourse] = useState()
   const [lessonCounter, setLessonCounter] = useState(0);
+  const history = useHistory()
 
   useEffect(() => {
     const fetchData = async () =>  {
@@ -28,12 +28,14 @@ function LessonPage() {
     up ? setLessonCounter(lessonCounter + 1) 
       : setLessonCounter(lessonCounter - 1) ;
   }
-
+  function backToCourse() {
+    history.push(`/courses/${courseSlug}`)
+  }
+  
   if(lessonCounter > 0) {
     return <Redirect to={`/courses/${courseSlug}/${course.lessons[lessonCounter].slug}`} />
   }
 
-  debugger;
   return (
     
     <Layout>
@@ -43,10 +45,12 @@ function LessonPage() {
           <>
             <LessonDetail {...lesson} />
             <div className="lesson-nav">
-                <Button onClick={() => navigate(false)}>Previous lesson</Button>
-                { course.lessons.length === lessonCounter
-                    ? null
-                    : <Button onClick={() => navigate(true)}>Next lesson</Button>
+                { lessonCounter === 0 
+                  ? <Button onClick={() => backToCourse()}>Back to Course presentation</Button>
+                  : <Button onClick={() => navigate(false)}>Previous lesson</Button>
+                }
+                { course.lessons.length !== (lessonCounter + 1) &&
+                    <Button onClick={() => navigate(true)}>Next lesson</Button>
                 }
             </div>
           </>
