9dd448f628ae93a9783baf683f71f116bd8c947f
JC <jcmpes@gmail.com>
2021-08-05 14:20:05 +0200

    feat: stripe payment

--- src/components/courses/CourseDetailPage/CoursePage.js
+++ src/components/courses/CourseDetailPage/CoursePage.js
@@ -7,36 +7,68 @@ import Layout from '../../layout/Layout';
 import { Button } from '../../shared';
 import CourseDetail from './CourseDetail';
 
+import StripeCheckout from 'react-stripe-checkout';
+require('dotenv').config();
+
 function CoursePage() {
   const { courseSlug } = useParams();
   const { loading } = useSelector(getUi);
   const [course, setCourse] = useState();
 
   React.useEffect(() => {
-    const fetchData = async () =>  {
-      setCourse(await getCourse(courseSlug))
-    }
-    fetchData()
+    const fetchData = async () => {
+      setCourse(await getCourse(courseSlug));
+    };
+    fetchData();
   }, [courseSlug]);
+
+  const makePayment = token => {
+    const body = {
+      token,
+      course,
+    }
+    const headers = {
+      "Content-Type": "application/json"
+    }
+    return fetch(`http://localhost:8282/payment`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body)
+    }).then(response => {
+      console.log("RESPONSE: ", response);
+      const {status} = response
+      console.log("STATUS: ", status);
+    }).catch( err => console.log(err))
+
+  }
 
   return (
     <Layout>
-        <div className="course-detail-page">
-          {loading && "I'm loading..."}
-          {course &&
-            <>
-              <CourseDetail {...course} />
-              <div className="lesson-nav">
-                { course.lessons.length > 0 ?
-                  <Link to={`/courses/${courseSlug}/${course.lessons[0].slug}`}>
-                    <Button>Go to course</Button>
-                  </Link>
-                  : null
-                }
-              </div>
-            </>
-          }
-        </div>
+      <div className="course-detail-page">
+        {loading && "I'm loading..."}
+        {course && (
+          <>
+            <CourseDetail {...course} />
+            <div className="lesson-nav">
+              {course.lessons.length > 0 ? (
+                <Link to={`/courses/${courseSlug}/${course.lessons[0].slug}`}>
+                  <Button>Go to course</Button>
+                </Link>
+              ) : null}
+            </div>
+            <StripeCheckout
+              stripeKey={process.env.REACT_APP_KEY}
+              token={makePayment}
+              name={course.title}
+              amount={course.price * 100}
+            >
+              <button className="btn-large pink">
+                Buy for {course.price}$
+              </button>
+            </StripeCheckout>
+          </>
+        )}
+      </div>
     </Layout>
   );
 }
