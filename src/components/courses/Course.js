bf46df2f63b95399dea6fc09c004a8fa17536c66
Manuel Hernandez <cmanuelhm87@gmail.com>
2021-08-12 18:29:33 +0200

    Feat: Course basic styles and footer

--- src/components/courses/Course.js
+++ src/components/courses/Course.js
@@ -8,6 +8,7 @@ import { getAuth } from '../../store/selectors';
 import { useTranslation } from 'react-i18next';
 import { addToCartAction } from '../../store/actions/purchase';
 import { toast } from 'react-toastify';
+import './Course.css';
 
 const Course = ({ course, faved, purchased, inCart }) => {
   // eslint-disable-next-line no-unused-vars
@@ -17,76 +18,116 @@ const Course = ({ course, faved, purchased, inCart }) => {
   const dispatch = useDispatch();
   const isAuthor = course.user.username === username;
   return (
-    <div className="course-wrapper" key={course._id}>
-      <br />
-      {t('Title')}:{' '}
-      <Link className="course-title" to={`/courses/${course.slug}`}>
-        {course.title}
-      </Link>
-      <br />
-      <img src={`${course.image}`} height="50" alt={`${course.title}`} />
-      <br />
-      {t('Description')}: {course.description}
-      <br />
-      {t('Category')}: {course.category.name}
-      <br />
-      {t('Price')}: {course.price}
-      <br />
-      {t('Created by')}{' '}
-      <Link to={`/courses-by/${course.user.username}`}>
-        {isAuthor ? t('me') : course.user.username}
-      </Link>{' '}
-      {t('at')}{' '}
-      {new Date(course.createdAt).toLocaleDateString(t('en-en'), {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-      })}
-      <br />
-      {isAuthor ? (
-        <div>
-          <Link to={`/edit/${course.slug}`}>✏️ Edit</Link>
-        </div>
-      ) : !purchased && !inCart ? (
-        <div
-          style={{ cursor: 'pointer' }}
-          onClick={() => {
-            if (!isLogged) {
-              const Msg = ({ closeToast, toastProps }) => (
-                <div>
-                  Please, <Link to="/login">log in</Link> to purchase.
-                </div>
-              );
-              toast.warning(<Msg />);
-            } else {
-              //dispatch(purchaseAction(course._id, '123456'));
-              dispatch(addToCartAction(course._id, course.title, course.price));
-            }
-          }}
-        >
-          <button>Comprar por {course.price} €</button>
-        </div>
-      ) : inCart ? (
-        <div>en carrito</div>
-      ) : (
-        <div>Comprado</div>
-      )}
-      <div
-        onClick={() => {
-          dispatch(
-            favoritesAction(course._id, !faved ? addFav : removeFav, !faved),
-          );
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12 mt-3">
+          <div className="card">
+            <div className="card-horizontal">
+              <div className="img-rounded-wrapper">
+                <img
+                  className="border border-dark"
+                  src={`${course.image}`}
+                  min-height="120"
+                  max-height="auto"
+                  // height="320"
+                  // width="240"
+                  alt={`${course.title}`}
+                />
+              </div>
+              <div className="card-body">
+                <p className="card-title">
+                  {/* {t('Title')}:{' '} */}
+                  <Link className="course-title" to={`/courses/${course.slug}`}>
+                    {course.title}
+                  </Link>
+                </p>
+                <p className="card-text description">
+                  {/* {t('Description')}: {course.description} */}
+                  {course.description}
+                </p>
+                <p className="card-text category">
+                  {t('Category')}: {course.category.name}
+                </p>
+                <p className="card-text price">
+                  {/* {t('Price')}: {course.price} */}
+                  {course.price} €
+                </p>
+                <p className="card-text created">
+                  {t('Created by')}{' '}
+                  <Link to={`/courses-by/${course.user.username}`}>
+                    {isAuthor ? t('me') : course.user.username}
+                  </Link>{' '}
+                  {t('at')}{' '}
+                  {new Date(course.createdAt).toLocaleDateString(t('en-en'), {
+                    weekday: 'long',
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                  })}
+                </p>
+                <br />
+                {isAuthor ? (
+                  <div>
+                    <Link to={`/edit/${course.slug}`}>✏️ Edit</Link>
+                  </div>
+                ) : !purchased && !inCart ? (
+                  <div
+                    style={{ cursor: 'pointer' }}
+                    onClick={() => {
+                      if (!isLogged) {
+                        const Msg = ({ closeToast, toastProps }) => (
+                          <div>
+                            Please, <Link to="/login">log in</Link> to purchase.
+                          </div>
+                        );
+                        toast.warning(<Msg />);
+                      } else {
+                        //dispatch(purchaseAction(course._id, '123456'));
+                        dispatch(
+                          addToCartAction(
+                            course._id,
+                            course.title,
+                            course.price,
+                          ),
+                        );
+                      }
+                    }}
+                  >
+                    <div className="button-conainer">
+                      <button className="curseButton">
+                        Comprar por {course.price} €
+                      </button>
+                    </div>
+                  </div>
+                ) : inCart ? (
+                  <div className="shopping-course">en carrito</div>
+                ) : (
+                  <div className="purchased-course">Comprado</div>
+                )}
+                <div
+                  onClick={() => {
+                    dispatch(
+                      favoritesAction(
+                        course._id,
+                        !faved ? addFav : removeFav,
+                        !faved,
+                      ),
+                    );
 
-          // if (!faved) addFav(course._id);
-          // else removeFav(course._id);
-        }}
-      >
-        {username && !isAuthor && (
-          <div style={{ cursor: 'pointer' }}>
-            {faved === true ? '❤️' : '💛'}
+                    // if (!faved) addFav(course._id);
+                    // else removeFav(course._id);
+                  }}
+                >
+                  {username && !isAuthor && (
+                    <div style={{ cursor: 'pointer' }}>
+                      {faved === true ? '❤️' : '🖤'}
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,13 +1,87 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
-import styles from './Footer.module.css';
+// import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/img/logowhite.png';
+import styles from './Footer.css';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 const Footer = ({ darkMode }) => {
-  const { t } = useTranslation(['global']);
+  // const { t } = useTranslation(['global']);
 
   return (
     <footer className={styles.footer} data-theme={darkMode ? 'dark' : 'light'}>
-        <p>{t('footer.footer')}</p>
+      {/* <p>{t('footer.footer')}</p> */}
+      <div className="footer-resposive">
+        <div className="container bottom_border">
+          <div className="row">
+            <div className=" col-sm-4 col-md col-sm-4  col-12 col">
+              <h5 className="headin5_amrc col_white_amrc pt2">Find us</h5>
+
+              <p className="mb10">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s
+              </p>
+              <p>
+                <i className="fa fa-location-arrow"></i> 9878/25 sec 9 rohini 35{' '}
+              </p>
+              <p>
+                <i className="fa fa-phone"></i> +91-9999878398{' '}
+              </p>
+              <p>
+                <i className="fa fa fa-envelope"></i> info@example.com{' '}
+              </p>
+            </div>
+
+            <div className=" col-sm-4 col-md  col-6 col">
+              <h5 className="headin5_amrc col_white_amrc pt2">Quick links</h5>
+
+              <ul className="footer_ul_amrc">
+                <li>
+                  <Link href="http://webenlance.com">Image Rectoucing</Link>
+                </li>
+                <li>
+                  <Link href="http://webenlance.com">Clipping Path</Link>
+                </li>
+
+                <li>
+                  <Link href="http://webenlance.com">
+                    Hair Masking/Clipping
+                  </Link>
+                </li>
+                <li>
+                  <Link href="http://webenlance.com">Image Cropping</Link>
+                </li>
+                <li>
+                  <section className="social mb-4">
+                    <Link>
+                      <FacebookIcon />
+                    </Link>
+
+                    <Link>
+                      <InstagramIcon />
+                    </Link>
+
+                    <Link>
+                      <GitHubIcon />
+                    </Link>
+
+                    <Link>
+                      <LinkedInIcon />
+                    </Link>
+                  </section>
+                </li>
+              </ul>
+            </div>
+            <div className=" col-sm-4 col-md  col-6 col">
+              <img className={styles.logo} src={logo} alt="logo" />
+            </div>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
