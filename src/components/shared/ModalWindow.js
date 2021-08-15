a428e48ac12cf090aeab6e6286d74ef041f36c4f
oscarCorb <info@oscarcorb.com>
2021-08-15 17:17:20 +0200

    feat: reusable modal window component

---
+++ src/components/shared/ModalWindow.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './ModalWindow.module.css';
+import closeIcon from '../../assets/svg/close.svg';
+
+// const ModalWindow = ({ title, isMenuOpen, closeMenu }) => {
+const ModalWindow = ({ title, isMenuOpen, closeMenu, children }) => {
+  return (
+    <div className={styles.modalContainer}>
+      <div className={styles.closeBtnContainer}>
+        <img src={closeIcon} onClick={closeMenu} alt="close icon" />
+      </div>
+
+      {title && <div className={styles.modalTitle}>{title}</div>}
+
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default ModalWindow;
