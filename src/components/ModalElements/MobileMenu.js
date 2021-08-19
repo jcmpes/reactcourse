fd5cdcf65da9ce56531e2aca86a4fa4b229c367e
Andrés <usuario616@gmail.com>
2021-08-20 00:30:18 +0200

    feat: modal mobile menu conditional items depending logged or not

--- src/components/ModalElements/MobileMenu.js
+++ src/components/ModalElements/MobileMenu.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
 import { logout } from '../../api/auth';
 import { authLogout } from '../../store/actions/logout';
 import ModalWindow from '../shared/ModalWindow';
 import styles from './MobileMenu.module.css';
+import { getIsLogged } from '../../store/selectors';
 
 const MobileMenu = ({ closeModal }) => {
   const { t } = useTranslation(['global']);
+
+  const isLogged = useSelector(getIsLogged);
 
   const dispatch = useDispatch();
   const history = useHistory();
@@ -26,27 +29,48 @@ const MobileMenu = ({ closeModal }) => {
         closeModal={closeModal}
         children={
           <div className={styles.mobileMenu} onClick={closeModal}>
-            <Link className={styles.link} onClick={closeModal} to="/register">
-              {t('mobile menu.register')}
-            </Link>
-            <Link className={styles.link} onClick={closeModal} to="/login">
-              {t('mobile menu.log in')}
-            </Link>
-            <Link className={styles.link} onClick={handleLogoutClick} to="/">
-              {t('mobile menu.logout')}
-            </Link>
-            <Link className={styles.link} onClick={closeModal} to="/courses">
-              {t('mobile menu.courses')}
-            </Link>
-            <Link className={styles.link} onClick={closeModal} to="/create">
-              {t('mobile menu.create course')}
-            </Link>
-            <Link className={styles.link} onClick={closeModal} to="/user">
-              {t('mobile menu.my account')}
-            </Link>
-            <Link className={styles.link} onClick={closeModal} to="/myfavs">
-              {t('mobile menu.my favourites')}
-            </Link>
+            {!isLogged && (
+              <>
+                <Link
+                  className={styles.link}
+                  onClick={closeModal}
+                  to="/register"
+                >
+                  {t('mobile menu.register')}
+                </Link>
+
+                <Link className={styles.link} onClick={closeModal} to="/login">
+                  {t('mobile menu.log in')}
+                </Link>
+              </>
+            )}
+            {isLogged && (
+              <>
+                <Link
+                  className={styles.link}
+                  onClick={handleLogoutClick}
+                  to="/"
+                >
+                  {t('mobile menu.logout')}
+                </Link>
+                <Link
+                  className={styles.link}
+                  onClick={closeModal}
+                  to="/courses"
+                >
+                  {t('mobile menu.courses')}
+                </Link>
+                <Link className={styles.link} onClick={closeModal} to="/create">
+                  {t('mobile menu.create course')}
+                </Link>
+                <Link className={styles.link} onClick={closeModal} to="/user">
+                  {t('mobile menu.my account')}
+                </Link>
+                <Link className={styles.link} onClick={closeModal} to="/myfavs">
+                  {t('mobile menu.my favourites')}
+                </Link>
+              </>
+            )}
             <Link className={styles.link} onClick={closeModal} to="/categories">
               {t('mobile menu.categories')}
             </Link>
