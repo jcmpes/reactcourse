2f8f37af6b1129939444dd869474a93cbefe720f
Manuel Hernandez <cmanuelhm87@gmail.com>
2021-08-25 10:58:02 +0200

    Fix: Fix positions for the NewCourseForm

--- src/components/courses/NewCoursePage/NewCourseForm.js
+++ src/components/courses/NewCoursePage/NewCourseForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { FormField, Button, Input } from '../../../components/shared';
 import FileUpload from '../../shared/FileUpload';
+import './NewCourseForm.css';
 
 function NewCourseForm({
   onSubmit,
@@ -35,87 +36,134 @@ function NewCourseForm({
       <h2>{t('course.course intro')}</h2>
       <div className="courseForm">
         <form onSubmit={onSubmit}>
-          <FormField
-            type="text"
-            label={'title'}
-            name="title"
-            placeholder="Title"
-            value={courseDetails.title}
-            onChange={handleChange}
-          />
-          <Input
-            as="select"
-            label={'category'}
-            name="category"
-            value={courseDetails.category}
-            onChange={handleChange}
-            options={[{ name: 'Select category', _id: '000' }, ...categories]}
-          />
-          <FormField
-            type={'text'}
-            label={'description'}
-            name="description"
-            placeholder="Description"
-            value={courseDetails.description}
-            onChange={handleChange}
-          />
-          <FormField
-            type={'text'}
-            label={'video'}
-            name="video"
-            placeholder="Video"
-            value={courseDetails.video}
-            onChange={handleChange}
-          />
-          <FormField
-            type={'number'}
-            label={'price'}
-            min="0"
-            max="1000"
-            name="price"
-            placeholder="Price"
-            value={courseDetails.price}
-            onChange={handleChange}
-          />
-          <FormField
-            type={'textarea'}
-            label={'content'}
-            name="content"
-            placeholder="Content"
-            value={courseDetails.content}
-            onChange={handleChange}
-          />
-          <FormField
-            type={'text'}
-            label={'requirements'}
-            name="requirements"
-            placeholder="Requirements"
-            value={courseDetails.requirements}
-            onChange={handleChange}
-          />
-          <FormField
-            type={'text'}
-            label={'whatYouWillLearn'}
-            name="whatYouWillLearn"
-            placeholder="What you will learn"
-            value={courseDetails.whatYouLearn}
-            onChange={handleChange}
-          />
-          <FormField
-            type={'number'}
-            label={'level'}
-            name="level"
-            min="0"
-            max="3"
-            placeholder="Level"
-            value={courseDetails.level}
-            onChange={handleChange}
-          />
-          <FileUpload
-            label={'image'}
-            courseDetails={courseDetails}
-            setCourseDetails={setCourseDetails}
-          />
+          <div className="row">
+            <div className="col-12 ">
+              <div className="row justify-content-md-center">
+                <div className="col-lg-3 col-md-6 col-sm-3">
+                  <FormField
+                    type="text"
+                    label={'title'}
+                    rows={8}
+                    cols={50}
+                    name="title"
+                    placeholder="Title"
+                    value={courseDetails.title}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div className="col-lg-3 col-md-6 col-sm-3">
+                  <FormField
+                    type={'text'}
+                    label={'video'}
+                    name="video"
+                    placeholder="Video"
+                    value={courseDetails.video}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="col-lg-3 col-md-6 col-sm-3">
+                  <FormField
+                    type={'number'}
+                    label={'price'}
+                    min="0"
+                    max="1000"
+                    name="price"
+                    placeholder="Price"
+                    value={courseDetails.price}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-12">
+              <div className="row justify-content-md-center textareaWrapper">
+                <div className="col-sm-3 col-md-6">
+                  <textarea
+                    type={'textarea'}
+                    label={'content'}
+                    rows={8}
+                    cols={50}
+                    name="content"
+                    placeholder="Content"
+                    value={courseDetails.content}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className=" col-md-6 col-sm-3">
+                  <textarea
+                    type={'text'}
+                    label={'description'}
+                    rows={8}
+                    cols={50}
+                    name="description"
+                    placeholder="Description"
+                    value={courseDetails.description}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className=" col-md-6 col-sm-3">
+                  <textarea
+                    type={'text'}
+                    label={'requirements'}
+                    name="requirements"
+                    rows={8}
+                    cols={50}
+                    placeholder="Requirements"
+                    value={courseDetails.requirements}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="col-sm-3 col-md-6">
+                  <textarea
+                    type={'text'}
+                    label={'whatYouWillLearn'}
+                    rows={8}
+                    cols={50}
+                    name="whatYouWillLearn"
+                    placeholder="What you will learn"
+                    value={courseDetails.whatYouLearn}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-12 mt-4">
+              <div className="row justify-content-md-center selectByCategory">
+                <div className="col-lg-2 col-md-6 col-sm-3 selectCategoryBarContainer">
+                  <Input
+                    className="selectCategoryBarForm"
+                    as="select"
+                    name="category"
+                    value={courseDetails.category}
+                    onChange={handleChange}
+                    options={[
+                      { name: 'Select category', _id: '000' },
+                      ...categories,
+                    ]}
+                  />
+                </div>
+                <div className="col-lg-2 col-md-6 col-sm-3">
+                  <FormField
+                    type={'number'}
+                    label={'level'}
+                    name="level"
+                    min="0"
+                    max="3"
+                    placeholder="Level"
+                    value={courseDetails.level}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="col-lg-2 col-md-6 col-sm-3">
+                  <FileUpload
+                    courseDetails={courseDetails}
+                    setCourseDetails={setCourseDetails}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
           <Button disabled={verifyForm()} type="submit">
             {t('submit')}
           </Button>
